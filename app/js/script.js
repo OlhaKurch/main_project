e714e83e0f0e6496c2a5de01fd2947bb1ba299ee
Olha <olga.kurch17@gmail.com>
2023-03-02 20:43:07 +0200

    add project

---
+++ app/js/script.js
@@ -0,0 +1,20 @@
+
+$('.autoplay').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  arrows: false,
+  dotsClass: 'achievements__slider-dots',
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings:{
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+    },
+  ]
+});
