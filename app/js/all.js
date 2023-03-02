81440e1391dfc83c2ab4f21f635ef50e54b5daeb
Olha <olga.kurch17@gmail.com>
2023-03-02 22:58:21 +0200

    add slick

--- app/js/all.js
+++ app/js/all.js
@@ -4105,3 +4105,22 @@ $('.autoplay').slick({
     }
   }]
 });
+$('.autoplay--story').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  arrows: false,
+  dotsClass: 'achievements__slider-dots',
+  mobileFirst: true
+  // responsive: [
+  //   {
+  //     breakpoint: 767,
+  //     settings:{
+  //         slidesToShow: 1,
+  //         slidesToScroll: 4,
+  //       }
+  //   },
+  // ]
+});
--- app/js/script.js
+++ app/js/script.js
@@ -18,3 +18,23 @@ $('.autoplay').slick({
     },
   ]
 });
+
+$('.autoplay--story').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  arrows: false,
+  dotsClass: 'achievements__slider-dots',
+  mobileFirst: true,
+  // responsive: [
+  //   {
+  //     breakpoint: 767,
+  //     settings:{
+  //         slidesToShow: 1,
+  //         slidesToScroll: 4,
+  //       }
+  //   },
+  // ]
+});
